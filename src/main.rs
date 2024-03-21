68f9c340ea148a5c68d6229f065eea152e9b5413
root <root@bluebox.vbox>
2024-03-21 13:24:03 -0400

    Login/Logout/Sessions

--- src/main.rs
+++ src/main.rs
@@ -9,20 +9,15 @@ use serde::{Serialize, Deserialize};
 use diesel::prelude::*;
 use diesel::pg::PgConnection;
 use dotenv::dotenv;
+use models::{User, NewUser, LoginUser};
+use actix_identity::{Identity, CookieIdentityPolicy, IdentityService};
 
-use models::{User, NewUser};
 
 #[derive(Serialize)]
 struct Post {
     title: String,
     link: String,
     author: String,
-}
-
-#[derive(Debug, Deserialize)]
-struct LoginUser {
-    username: String,
-    password: String,
 }
 
 #[derive(Debug, Deserialize)]
@@ -41,6 +36,11 @@ fn establish_connection() -> PgConnection {
         .expect(&format!("Error connecting to {}", database_url))
 }
 
+async fn logout(id: Identity) -> impl Responder {
+    id.forget();
+    HttpResponse::Ok().body("Logged Out")
+}
+
 async fn submission(tera: web::Data<Tera>) -> impl Responder {
     let mut data = Context::new();
     data.insert("title", "Submit a post");
@@ -51,16 +51,38 @@ async fn process_submission(data: web::Form<Submission>) -> impl Responder {
     println!("{:?}", data);
     HttpResponse::Ok().body(format!("Posted submission: {}", data.title))
 }
-async fn login(tera: web::Data<Tera>) -> impl Responder {
+async fn login(tera: web::Data<Tera>, id: Identity) -> impl Responder {
     let mut data = Context::new();
     data.insert("title", "Login");
 
+    if let Some(id) = id.identity() {
+        return HttpResponse::Ok().body("Already logged in")
+    }
     let rendered = tera.render("login.html", &data).unwrap();
     HttpResponse::Ok().body(rendered)
 }
-async fn process_login(data: web::Form<LoginUser>) -> impl Responder {
-    println!("{:?}", data);
-    HttpResponse::Ok().body(format!("Logged in: {}", data.username))
+async fn process_login(data: web::Form<LoginUser>, id: Identity) -> impl Responder {
+    use schema::users::dsl::{username, users};
+
+    let connection = establish_connection();
+    let user = users.filter(username.eq(&data.username)).first::<User>(&connection);
+
+    match user {
+        Ok(u) => {
+            if u.password == data.password {
+                let session_token = String::from(u.username);
+                id.remember(session_token);
+                HttpResponse::Ok().body(format!("Logged in: {}", data.username))
+            } else {
+                HttpResponse::Ok().body("Password is incorrect.")
+            }
+        },
+        Err(e) => {
+            println!("{:?}", e);
+            //REMOVE BEFORE PRODUCTION
+            HttpResponse::Ok().body("User does not exist")
+        }
+    }
 }
 async fn process_signup(data: web::Form<NewUser>) -> impl Responder {
     use schema::users;
@@ -110,17 +132,24 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         let tera = Tera::new("templates/**/*").unwrap();
         App::new()
-        .data(tera)
-        .route("/", web::get().to(index))
-        .route("/signup", web::get().to(signup))
-        .route("/signup", web::post().to(process_signup))
-        .route("/login", web::get().to(login))
-        .route("/login", web::post().to(process_login))
-        .route("/submission", web::get().to(submission))
-        .route("/submission", web::post().to(process_submission))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
-
+                .wrap(IdentityService::new(
+                    CookieIdentityPolicy::new(&[0;32])
+                    .name("auth-cookie")
+                    .secure(false)
+                    )
+                )
+                .data(tera)
+                .route("/", web::get().to(index))
+                .route("/signup", web::get().to(signup))
+                .route("/signup", web::post().to(process_signup))
+                .route("/login", web::get().to(login))
+                .route("/login", web::post().to(process_login))
+                .route("/submission", web::get().to(submission))
+                .route("/submission", web::post().to(process_submission))
+                .route("/logout", web::post().to(process_login))
+                .route("/logout", web::to(logout))
+            })
+            .bind("127.0.0.1:8000")?
+            .run()
+            .await
 }
--- src/models.rs
+++ src/models.rs
@@ -10,12 +10,16 @@ pub struct User {
     pub password: String,
 }
 
-
-
 #[derive(Debug, Deserialize, Insertable)]
 #[table_name="users"]
 pub struct NewUser {
     pub username: String,
     pub email: String,
+    pub password: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct LoginUser {
+    pub username: String, 
     pub password: String,
 }
